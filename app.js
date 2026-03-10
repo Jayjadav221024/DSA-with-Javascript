13cfe8386be530511941eb6e1526ba236dc1da47
Jay Jadav <jayjadav9313@gmail.com>
2026-03-10 10:20:34 +0530

    add code

---
+++ app.js
@@ -0,0 +1,40 @@
+// sum of two integer
+let a = 10;
+let b = 20;
+console.log(a+b);
+
+// sum of integer + string
+let one = 12;
+let two = "13";
+console.log(one+two);
+
+//sum of sum + message
+let msg1 = 12;
+let msg2 = 13;
+console.log("sum of"+msg1+msg2);
+
+//type correction  inteeveiw question process name type correction
+console.log("1"+1); //concetinate
+console.log("1"-1); //subtract why becuase the - sign only for subtract
+console.log("1"*2); //maltiply why becus the * sign only for multiply
+console.log("1"/1); //devide why becuse the / sign only work for devide
+
+//accept and print the answer
+let age = prompt("what is your age");  //prompt convet any value in string
+age = Number(age);
+console.log(typeof age);
+
+//type casting
+//string to number
+//number to string
+//char to staring
+//any one type to another type convertion that called as th type casting
+
+//swapping two varivable via 3 method
+let a1 = 101;
+let b2 = 202;
+let c ;
+c = a1; //c = 10 & a = 10
+a1 = b2; //a = 20 & b = 20
+b2 = c; //b = 10 & c = 10
+console.log(a1,b2);
